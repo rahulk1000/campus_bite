8f3de722e3734d709beace1ac823267ef5de0308
Rahul <rahulkapadia@umail.ucsb.edu>
2012-09-09 00:06:17 -0700

    Initialized project, updated Gemfile to include rspec-rails, annotate, and machanize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampusBite::Application.config.secret_token = 'e685cdae8dd1c86f07d61542f561837a0b1a64da5b27166de1e74158cf4261667637e8c82f76d03cd7f307a2775c2408a2643738656f22eb6ddaffc434e92b4b'
