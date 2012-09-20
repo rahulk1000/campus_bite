adaf73611ff0e26edcc2cef33d10601bf35c2afd
Rahul <rahulkapadia@umail.ucsb.edu>
2012-09-20 00:23:39 -0700

    added code to extract information regarding meals from daily menu pages

--- lib/tasks/scrape_menus.rake
+++ lib/tasks/scrape_menus.rake
@@ -1,6 +1,7 @@
 require_relative 'scrape_menus_helper'
 desc 'Scrapes menu items from all the dining commons at UCSB'
 task :scrape_menus => :environment do
+  $f = File.open("#{Rails.root}/lib/tasks/output.txt", 'w')
   menu_home_file = File.open("#{Rails.root}/lib/tasks/stub_pages/menu_home_page.html")
   menu_home_doc = Nokogiri::HTML(menu_home_file)
   menu_home_file.close
@@ -9,16 +10,17 @@ task :scrape_menus => :environment do
     a.user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:13.0) Gecko/20100101 Firefox/13.0.1'
   end
   # Fetch menu home page. Also sets session cookie in 'browser'
-  browser.get('http://netnutrition.housing.ucsb.edu/NetNutrition/Home.aspx') 
+  #browser.get('http://netnutrition.housing.ucsb.edu/NetNutrition/Home.aspx') 
   
   dining_commons_nodeset = menu_home_doc.xpath('//ul[@class="cbo_nn_unitTreeListDiv"]' +
                                                 '/li[@class="cbo_nn_unitTreeParent"]')  
   throw "Error scraping information about the dining commons" if dining_commons_nodeset.size != 4
   # Iterate through the each dining common
+=begin
   dining_commons_nodeset.each do |dining_common_node|
     # Extract dining common's name and convert it into a symbol
-    dining_common_sym = dining_common_node.child.content.strip.to_symbol
-    unless [:ortega, :de_la_guerra, :carrillo, :portola].include?(dining_common_sym)
+    dining_common_name = dining_common_node.child.content.strip
+    unless ['Ortega', 'De La Guerra', 'Carrillo', 'Portola'].include?(dining_common_name)
       throw "invalid dining common symbol" 
     end
     categories_nodeset = dining_common_node.xpath('./ul/li[@class="cbo_nn_unitTreeParentNoChild"]')
@@ -30,12 +32,15 @@ task :scrape_menus => :environment do
       category_name = category_href_element.content.strip
       # Converting "(Dining Common name)'s Daily Menu" to simply "Daily Menu"
       category_name = 'Daily Menu'  if category_name.include?('Daily Menu') 
-      category_sym = category_name.to_symbol
-      http_request_codes = category_href_element[:href].strip.scan(/\d+/)     
-      route(dining_common_sym, category_sym, browser, http_request_codes)
+      http_request_codes = category_href_element[:href].strip.scan(/\d+/)
+      route(dining_common_name, category_name, browser, http_request_codes)
     end
   end
-  #scrape_salad_bar(:ortega, :salad_bar, [])
+=end
+  route('Ortega', 'Daily Menu', browser, [1, 2])
+ $f.close
 end
+
+
 
 
--- lib/tasks/scrape_menus_helper.rb
+++ lib/tasks/scrape_menus_helper.rb
@@ -14,56 +14,76 @@ class Nokogiri::XML::NodeSet
         subcategory_name = current_node.content.strip
       elsif current_node[:class] == 'cbo_nn_itemHover'
         food_item_name = current_node.content.strip 
-        yield(food_item_name, subcategory_name)
+        yield food_item_name, subcategory_name
       end
     end
   end 
+  
+  def each_meal
+    self.each do |meal_info_node|
+      meal_date = meal_info_node.at_xpath("./tr[1]/td/text()").content.to_date
+      meal_info_node.xpath("./tr[2]//a[@class='cbo_nn_menuLink']").each do |meal_type_href_element|
+        meal_type = meal_type_href_element.content.strip
+        meal_request_codes = meal_type_href_element[:href].strip.scan(/\d+/)
+        yield meal_date, meal_type, meal_request_codes
+      end
+    end   
+  end
+  
 end
 
-def route(dining_common, category, browser, http_request_codes)  
-  case category
-  when :daily_menu then scrape_daily_menu(dining_common, category, http_request_codes) 
-  when :salad_bar then scrape_salad_bar(dining_common, category, browser, http_request_codes) 
-  when :condiments then scrape_condiments(dining_common, category, http_request_codes) 
-  when :breads_and_cereals then scrape_breads_and_cereals(dining_common, category, http_request_codes) 
-  when :beverages then scrape_beverages(dining_common, category, http_request_codes) 
-  else throw "Invalid category being processsed"
+def route(dining_common_name, category_name, browser, http_request_codes) 
+  case category_name
+  when 'Salad Bar', 'Condiments', 'Breads and Cereals', 'Beverages' 
+    #fetch_food_items(dining_common_name, category_name, browser, http_request_codes)   
+  when 'Daily Menu' 
+    puts category_name.to_s.red
+    daily_menu_file = File.open("#{Rails.root}/lib/tasks/stub_pages/ortega_daily_menu.html", 'r')
+    daily_menu_doc = Nokogiri::HTML(daily_menu_file)
+    daily_menu_file.close
+    meals_info_nodeset = daily_menu_doc.xpath("//table[@class='cbo_nn_menuTable']/tr/" +
+                                              "td[@class='cbo_nn_menuCell']/table[tr[last()=2]]")
+    throw "couldnt fetch information on meals" if meals_info_nodeset.empty?
+    meals_info_nodeset.each_meal do |meal_date, meal_type, meal_request_codes|
+      p "#{meal_date}, #{meal_type}, #{meal_request_codes}"
+    end
+  else
+    throw "Invalid category" 
   end
 end
 
-def fetch_food_item_page(browser, http_request_codes)
-  food_item_page = browser.post('http://netnutrition.housing.ucsb.edu/NetNutrition/Menu.aspx/OpenMenu', 
+def fetch_food_items(dining_common_name, category_name, browser, http_request_codes)
+  food_items_page = browser.post('http://netnutrition.housing.ucsb.edu/NetNutrition/Menu.aspx/OpenMenu', 
                                 { unitOid: http_request_codes[0], menuOid: http_request_codes[1] })   
-end
 
-def scrape_daily_menu(dining_common, category, http_request_codes) 
-end
-
-def scrape_salad_bar(dining_common, category, browser, http_request_codes) 
-  salad_bar_page = fetch_food_item_page(browser, http_request_codes)
 =begin
   salar_bar_file = File.open("#{Rails.root}/lib/tasks/stub_pages/ortega_condiments.html")
   salar_bar_doc = Nokogiri::HTML(salar_bar_file)
   salar_bar_file.close
 =end
-  subcategories_and_items_nodeset = salad_bar_page.search('//table[@class="cbo_nn_itemGridTable"]' +
-                                                          '/tr/td[@class="cbo_nn_itemGroupRow" or' + 
-                                                          '@class="cbo_nn_itemHover"]')
+  
+ # subcategories_and_items_nodeset = salar_bar_doc.xpath('//table[@class="cbo_nn_itemGridTable"]' +
+                                #                           '/tr/td[@class="cbo_nn_itemGroupRow" or' + 
+                                 #                          '@class="cbo_nn_itemHover"]')
+
+  subcategories_and_items_nodeset = food_items_page.search('//table[@class="cbo_nn_itemGridTable"]' +
+                                                           '/tr/td[@class="cbo_nn_itemGroupRow" or' + 
+                                                           '@class="cbo_nn_itemHover"]')
   throw "couldnt fetch food items and their subcatagories" if subcategories_and_items_nodeset.empty?
-  puts "Salad Bar for #{dining_common}".blue
+  $f.puts("\n\n#{category_name} for #{dining_common_name}")
   subcategories_and_items_nodeset.each_food_item do |food_item_name, subcategory_name|
-    puts "#{food_item_name}, " + "#{subcategory_name}".yellow
-  end                                               
+  $f.puts("\t#{food_item_name}, #{subcategory_name}")
+  end
 end
 
-def scrape_condiments(dining_common, category, http_request_codes) 
-
-end
-
-def scrape_breads_and_cereals(dining_common, category, http_request_codes) 
+def fetch_food_item_page(browser, http_request_codes)
 
 end
 
-def scrape_beverages(dining_common, category, http_request_codes) 
 
+def scrape_salad_bar(dining_common, category, browser, http_request_codes) 
+                                             
 end
+# TODO: sanitize function for scraped content (removing non breaking spaced etc)
+# TODO: place all class extesions somewhere else?
+
