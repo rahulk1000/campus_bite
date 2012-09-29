2a2a7895fdeb336c43970267417dd3bfd1c876f2
Rahul <rahulkapadia@umail.ucsb.edu>
2012-09-29 04:45:37 -0700

    finished integrating controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   def home
+    if params[:search].nil?
+      render inline: "", layout: 'application'  
+    else
+      @food_items = params[:search].blank? ? [] : FoodItem.search_by_name(params[:search]).where("date >= ? OR date IS ?", Date.today, nil)
+    end
   end
   def items
   end
 end
+
