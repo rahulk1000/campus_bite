305f44a35d6a75b7284bb4366e534cf23f12f86c
Rahul <rahulkapadia@umail.ucsb.edu>
2012-09-30 15:35:38 -0700

    added sorting functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,28 @@
 class PagesController < ApplicationController
   respond_to :html, :js
+  helper_method :sort_column, :sort_direction
   def home
     @food_items = if params[:search].blank?
                     [ ]
                   else
-                    FoodItem.search_by_name(params[:search]).
-                             where("date >= ? OR date IS ?", Date.today, nil).
-                            order("name ASC")
+                    FoodItem.order(sort_column + ' ' + sort_direction).
+                             search_by_name(params[:search]).
+                             where("date >= ? OR date IS ?", Date.today, nil)                            
                   end
                 
     respond_with(@food_items)
   end
+  
   def items
+    
   end
+  
+  private
+  def sort_column
+    %w[name date dining_common].include?(params[:sort]) ? params[:sort] : 'date'
+  end
+  def sort_direction 
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
+  end   
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def sort_link(column, title)
+    if column == sort_column
+      arrow_icon = if sort_direction == 'asc'  
+                      content_tag(:i,nil,class: 'icon-arrow-up')
+                    else
+                      content_tag(:i,nil,class: 'icon-arrow-down')
+                    end
+
+    end
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to({ search: params[:search], sort: column, direction: direction }, remote: true) do
+      arrow_icon.to_s + title +  arrow_icon.to_s
+    end 
+  end
 end
